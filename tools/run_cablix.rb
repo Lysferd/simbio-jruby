1cc1655030ffc8077c086b65f60e227d786ed848
Lysferd <phabio_almeida@hotmail.com>
2017-07-21 23:55:34 -0300

    Adding readme.
    - Added missing `README` file (though not in accordance with Github Markdown standard);
    - Small changes on the Cablix tool code.

--- tools/run_cablix.rb
+++ tools/run_cablix.rb
@@ -23,15 +23,16 @@
 # along with SimBio-JRuby.  If not, see <http://www.gnu.org/licenses/>.
 #==============================================================================
 
-$LOAD_PATH.push "#{File.dirname(__FILE__)}/lib"
-$LOAD_PATH.push "#{File.dirname(__FILE__)}/javalib"
-require 'simbio_jruby'
+$LOAD_PATH.push Dir::pwd + "/lib"
+$LOAD_PATH.push Dir::pwd + "/javalib"
+require 'simbio_jruby.rb'
 
-$Verbose = true
+$Verbose = false
 $stdout.reopen "out/cablix/log (#{Time::now.strftime('%Y-%m-%d')})", ?w
 $stderr.reopen "out/cablix/error (#{Time::now.strftime('%Y-%m-%d')})", ?w
 
 input = -5e3
+step = -1e3
 
 [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 2000, 3000, 4000, 5000 ].each do | grid |
   loop do
@@ -47,7 +48,7 @@ input = -5e3
       if t.update
         break
       else
-        input += -5e3
+        input += step
       end
       
     rescue RangeError
